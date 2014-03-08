1cd345c2d7aa06556d1573d6a16208ab975ff3ab
jsxhhyf <jsxhhyf@gmail.com>
2014-03-08 20:36:21 +0800

    test ssh push
    Signed-off-by: jsxhhyf <jsxhhyf@gmail.com>

--- FIT5183A1/src/main/Main.java
+++ FIT5183A1/src/main/Main.java
@@ -14,7 +14,8 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("hello github and this is for test");
+		System.out.println("this is for another test");
 	}
 
 }
