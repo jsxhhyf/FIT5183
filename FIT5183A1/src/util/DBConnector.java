be7b9ca965194074f3dcf08fc68caf68d5388012
jsxhhyf <jsxhhyf@gmail.com>
2014-03-09 16:19:45 +0800

    initial version of DBConnector.java
    Signed-off-by: jsxhhyf <jsxhhyf@gmail.com>

--- FIT5183A1/src/util/DBConnector.java
+++ FIT5183A1/src/util/DBConnector.java
@@ -6,6 +6,8 @@
  */
 package util;
 
+import java.sql.*;
+
 /**
  * @author Phillip
  *
@@ -15,9 +17,75 @@ public class DBConnector {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) { //for unit test
 		// TODO Auto-generated method stub
 
 	}
-
+	
+	
+	/**
+	 * build up the connection to the database and it is a static method
+	 * @param sqlString
+	 * @return the connection build up to the database based on sqlString 
+	 */
+	public static Connection connect(String sqlString) {
+		
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver"); //throw ClassNotFoundException
+			connection = DriverManager.getConnection(sqlString, "fit5183a1", ""); //throw SQLException
+		} catch (Exception e) { // combine two different exceptions
+			// TODO: handle exception
+			System.out.println("Fail to build connection!");
+		}
+		return connection;
+	}
+	
+	/**
+	 * execute the corresponding query according to the queryString
+	 * @param queryString
+	 * @return the query result
+	 */
+	public static ResultSet query(Connection connection, String queryString) {
+		
+		ResultSet resultSet = null;
+		
+		if (connection == null) {
+			System.out.println("Please establish connection first!");
+			return null;
+		}
+		try {
+			Statement statement = connection.createStatement();
+			resultSet = statement.executeQuery(queryString);
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		return resultSet;
+		
+	}
+	
+	public static int update(Connection connection, String queryString) {
+		
+		//ResultSet resultSet = null;
+		int rows = 0;
+		
+		if (connection == null) {
+			System.out.println("Please establish connection first!");
+			return 0;
+		}
+		try {
+			Statement statement = connection.createStatement();
+			rows = statement.executeUpdate(queryString);
+			
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return rows;
+		
+	}
+	
+	
 }
