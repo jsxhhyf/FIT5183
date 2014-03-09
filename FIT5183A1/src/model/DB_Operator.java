5a9ac5e231a33b41b80eebb7659be78f49ca2b03
jsxhhyf <jsxhhyf@gmail.com>
2014-03-10 01:03:24 +0800

    finish all kinds of query functions in DB_Operator
    Signed-off-by: jsxhhyf <jsxhhyf@gmail.com>

--- FIT5183A1/src/model/DB_Operator.java
+++ FIT5183A1/src/model/DB_Operator.java
@@ -31,7 +31,10 @@ public class DB_Operator {
 		// TODO Auto-generated method stub
 		DB_Operator db_Operator = new DB_Operator();
 		Flightinfo f = new Flightinfo();
-		f = db_Operator.queryFlighByNo("CA1220", "airline1");
+
+		f = db_Operator.queryFlightByLocaAndDateAndTime("C_CITY1", "A_CITY1",
+				"2014-03-01", "00:00", "20:00", "airline1");
+
 		if (db_Operator.connection != null) {
 			try {
 				db_Operator.connection.close();
@@ -40,16 +43,19 @@ public class DB_Operator {
 				e.printStackTrace();
 			}
 		}
-		Util.debug(f.getDeptAirportString());
+		if (f != null) {
+			Util.debug(f.getDeptAirportString());
+		}
 
 	}
 
 	private final String QUERY_STRING = "select * from ";
-	private final String QUERY_BY_NUMBER_STRING = " where FLNO = '";
+	// private final String QUERY_BY_NUMBER_STRING = " where FLNO = '";
 
 	private Connection connection;
-	private ResultSet resultSet;
-	private Flightinfo flightInfo;
+
+	// private ResultSet resultSet;
+	// private Flightinfo flightInfo;
 
 	/**
 	 * non-parameter constructor, initialize the connection to database
@@ -57,8 +63,8 @@ public class DB_Operator {
 	public DB_Operator() {
 		// TODO Auto-generated constructor stub
 		connection = DBConnector.connect(DBConnector.CONNECT_STRING);
-		resultSet = null;
-		flightInfo = new Flightinfo();
+		// resultSet = null;
+		// flightInfo = new Flightinfo();
 	}
 
 	/**
@@ -70,16 +76,109 @@ public class DB_Operator {
 	 */
 	public Flightinfo queryFlighByNo(String flightNoString, String tableString) {
 
+		Flightinfo flightInfo = null;
+		ResultSet resultSet = null;
+
 		if (flightNoString.length() != 6) { // validate the flight number
 			Util.debug("Wrong format of flight number!");
 			return null;
 		} else {
 			resultSet = DBConnector.query(connection, QUERY_STRING
-					+ tableString + QUERY_BY_NUMBER_STRING + flightNoString
-					+ "'");
-			this.flightInfo = transformToFlightInfo(resultSet);
-			return this.flightInfo;
+					+ tableString + "where FLNO = '" + flightNoString + "'");
+			flightInfo = transformToFlightInfo(resultSet);
+			return flightInfo;
 		}
+	}
+
+	/**
+	 * query flight by departing city
+	 * 
+	 * @param deptString
+	 * @param tableString
+	 * @return the corresponding flight info
+	 */
+	public Flightinfo queryFlightByDept(String deptString, String tableString) {
+
+		Flightinfo flightinfo = null;
+		ResultSet resultSet = null;
+
+		resultSet = DBConnector.query(connection, QUERY_STRING + tableString
+				+ "where DPCT = '" + deptString + "'");
+		flightinfo = transformToFlightInfo(resultSet);
+
+		return flightinfo;
+	}
+
+	/**
+	 * query flight by departing and arriving cities
+	 * 
+	 * @param deptString
+	 * @param destString
+	 * @param tableString
+	 * @return the corresponding flight info
+	 */
+	public Flightinfo queryFlightByLocation(String deptString,
+			String destString, String tableString) {
+
+		Flightinfo flightinfo = null;
+		ResultSet resultSet = null;
+
+		resultSet = DBConnector.query(connection, QUERY_STRING + tableString
+				+ "where DPCT = '" + deptString + "' and DSCT = '" + destString
+				+ "'");
+		flightinfo = transformToFlightInfo(resultSet);
+
+		return flightinfo;
+	}
+
+	/**
+	 * query flight by departing and arriving cities and the departing date
+	 * 
+	 * @param deptString
+	 * @param destString
+	 * @param dateString
+	 * @param tableString
+	 * @return the corresponding flight info
+	 */
+	public Flightinfo queryFlightByLocaAndDate(String deptString,
+			String destString, String dateString, String tableString) {
+
+		Flightinfo flightinfo = null;
+		ResultSet resultSet = null;
+
+		resultSet = DBConnector.query(connection, QUERY_STRING + tableString
+				+ " where DPCT = '" + deptString + "' and DSCT = '"
+				+ destString + "' and TO_DAYS(DPDT) = TO_DAYS('" + dateString
+				+ "')");
+		flightinfo = transformToFlightInfo(resultSet);
+
+		return flightinfo;
+	}
+
+	public Flightinfo queryFlightByLocaAndDateAndTime(String deptString,
+			String destString, String dateString, String timeString1,
+			String timeString2, String tableString) {
+
+		Flightinfo flightinfo = null;
+		ResultSet resultSet = null;
+
+		resultSet = DBConnector
+				.query(connection,
+						QUERY_STRING
+								+ tableString
+								+ " where DPCT = '"
+								+ deptString
+								+ "' and DSCT = '"
+								+ destString
+								+ "' and UNIX_TIMESTAMP(CONCAT(DPDT,' ',DPTM)) < UNIX_TIMESTAMP('"
+								+ dateString
+								+ " "
+								+ timeString2
+								+ "') and UNIX_TIMESTAMP(CONCAT(DPDT,' ',DPTM)) > UNIX_TIMESTAMP('"
+								+ dateString + " " + timeString1 + "')");
+		flightinfo = transformToFlightInfo(resultSet);
+
+		return flightinfo;
 	}
 
 	/**
