15804ffb8525786ef81f5c01a3839f0272b8f2d2
jsxhhyf <jsxhhyf@gmail.com>
2014-03-12 21:07:18 +0800

    add a logical client
    Signed-off-by: jsxhhyf <jsxhhyf@gmail.com>

--- FIT5183A1/src/controller/Client.java
+++ FIT5183A1/src/controller/Client.java
@@ -6,11 +6,28 @@
  */
 package controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import util.Util;
+
 /**
  * @author Phillip
- *
+ * 
  */
 public class Client {
+
+	private final int PORT = 10010;
+	private Socket clientSocket = null;
+	private BufferedReader reader;
+	private PrintStream printer;
+	private String resultString;
 
 	/**
 	 * @param args
@@ -18,6 +35,93 @@ public class Client {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+
+	}
+
+	/**
+	 * default constructor initialize the socket and input output streams
+	 */
+	public Client() {
+
+		InetAddress address = null;
+		try {
+			address = InetAddress.getByName("localhost");
+			Util.debug(address.toString());
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			setClientSocket(new Socket(address, PORT));
+			reader = new BufferedReader(new InputStreamReader(
+					clientSocket.getInputStream()));
+			printer = new PrintStream(clientSocket.getOutputStream());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * @return the reader
+	 */
+	public BufferedReader getReader() {
+		return reader;
+	}
+
+	/**
+	 * @param reader the reader to set
+	 */
+	public void setReader(BufferedReader reader) {
+		this.reader = reader;
+	}
+
+	/**
+	 * @return the printer
+	 */
+	public PrintStream getPrinter() {
+		return printer;
+	}
+
+	/**
+	 * @param printer the printer to set
+	 */
+	public void setPrinter(PrintStream printer) {
+		this.printer = printer;
+	}
+
+	public Socket getClientSocket() {
+		return clientSocket;
+	}
+
+	public void setClientSocket(Socket clientSocket) {
+		this.clientSocket = clientSocket;
+	}
+
+	/**
+	 * @return the resultString
+	 */
+	public String getResultString() {
+		return resultString;
+	}
+
+	/**
+	 * @param resultString the resultString to set
+	 */
+	public void setResultString(String resultString) {
+		this.resultString = resultString;
+	}
+
+	public void query(String queryString) {
+		printer.println(queryString);
+		try {
+			setResultString(reader.readLine());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- FIT5183A1/src/view/ClientGUI.java
+++ FIT5183A1/src/view/ClientGUI.java
@@ -6,11 +6,25 @@
  */
 package view;
 
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.table.DefaultTableModel;
+
 /**
  * @author Phillip
- *
+ * 
  */
-public class ClientGUI {
+public class ClientGUI extends JFrame {
+
+	String[] columnName = { "FlightNumber", "DepartingCity",
+			"DepartingAirport", "DestinationCity", "DestinationAirport",
+			"DepartingDate", "ArrivingDate", "DepartingTime", "ArrivingTime",
+			"Class", "Price", "AvaliableSeats" };
+	DefaultTableModel tableModel = new DefaultTableModel(columnName,0);
+	
+	private JLabel jLabel;
 
 	/**
 	 * @param args
@@ -18,6 +32,25 @@ public class ClientGUI {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		ClientGUI clientGUI = new ClientGUI();
+	}
+
+	public ClientGUI() {
+		this.setTitle("MY FLIGHT BOOKING¡¡CENTRE");
+//		this.setLayout(new FlowLayout());
+		this.setSize(300, 200);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.add(getJLabel(),null);
+	}
+	
+	public JLabel getJLabel() {
+		if (jLabel == null) {
+			jLabel = new JLabel();
+			jLabel.setBounds(34,49,53,18);
+			jLabel.setText("Flight Number");
+		}
+		return jLabel;
 	}
 
 }
